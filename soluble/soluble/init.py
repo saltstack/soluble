3498e441bb45a58025dd35220a363f165394c44a
Tyler Levy Conde <tyler.levy-conde@broadcom.com>
2024-08-19 13:18:37 -0600

    more complete

--- soluble/soluble/init.py
+++ soluble/soluble/init.py
@@ -3,13 +3,10 @@ import sys
 
 def cli(hub):
     hub.pop.config.load(["soluble"], cli="soluble")
-    # Your app's options can now be found under hub.OPT.soluble
     kwargs = dict(hub.OPT.soluble)
 
-    # Initialize the asyncio event loop
     hub.pop.loop.create()
 
-    # Start the async code
     coroutine = hub.soluble.init.run(**kwargs)
     retcode = hub.pop.Loop.run_until_complete(coroutine)
     sys.exit(retcode)
