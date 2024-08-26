ffd67f4abc4b6cc19536ee7058405721c9bf4d47
Tyler Levy Conde <tyler.levy-conde@broadcom.com>
2024-08-26 16:21:06 -0600

    Better github workflows

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,31 +1,15 @@
 from unittest import mock
 
+import pop.hub
 import pytest
 
 
 @pytest.fixture(scope="session", name="hub")
-def integration_hub(hub):
-    for dyne in ["soluble"]:
-        hub.pop.sub.add(dyne_name=dyne)
+def integration_hub():
+    hub = pop.hub.Hub()
+    hub.pop.sub.add(dyne_name="soluble")
 
     with mock.patch("sys.argv", ["soluble"]):
         hub.pop.config.load(["soluble"], cli="soluble")
 
     yield hub
-
-
-async def _setup_hub(hub):
-    # Set up the hub before each function here
-    ...
-
-
-async def _teardown_hub(hub):
-    # Clean up the hub after each function here
-    ...
-
-
-@pytest.fixture(scope="function", autouse=True)
-async def function_hub_wrapper(hub):
-    await _setup_hub(hub)
-    yield
-    await _teardown_hub(hub)
