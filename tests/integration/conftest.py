6559162bf6e6fe1352661d470b9efa8ab9f9ccb2
Tyler Levy Conde <tyler.levy-conde@broadcom.com>
2024-08-26 17:29:51 -0600

    Setup with hatch

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,12 +1,10 @@
 from unittest import mock
 
-import pop.hub
 import pytest
 
 
 @pytest.fixture(scope="session", name="hub")
-def integration_hub():
-    hub = pop.hub.Hub()
+def integration_hub(hub):
     hub.pop.sub.add(dyne_name="soluble")
 
     with mock.patch("sys.argv", ["soluble"]):
