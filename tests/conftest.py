ae1c88113a0dcbc4384cbc5acb2dfc9f0dcc6f12
Tyler Levy Conde <tyler.levy-conde@broadcom.com>
2024-08-26 18:00:46 -0600

    Don't parse cli for config

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,6 @@ def integration_hub(hub):
     hub.pop.sub.add(dyne_name="soluble")
 
     with mock.patch("sys.argv", ["soluble"]):
-        hub.pop.config.load(["soluble"], cli="soluble")
+        hub.pop.config.load(["soluble"], cli="soluble", parse_cli=False)
 
     yield hub
